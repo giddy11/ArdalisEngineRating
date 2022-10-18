9279d473a44a2eca81762aa8cdedd37d85db3dfc
giddy11 <edoghotugiddy@gmail.com>
2022-10-18 15:42:47 +0100

    reflection replaced switch statement in raterfactory class

--- ArdalisRatingService/RaterFactory.cs
+++ ArdalisRatingService/RaterFactory.cs
@@ -10,25 +10,44 @@ namespace ArdalisRatingService
     {
         public Rater Create(Policy policy, RatingEngine engine)
         {
-            switch (policy.Type)
+            try
             {
-                case PolicyType.Auto:
-                    return new AutoPolicyRater(engine, engine.Logger);
-
-                case PolicyType.Land:
-                    return new LandPolicyRater(engine, engine.Logger);
-
-                case PolicyType.Life:
-                    return new LifePolicyRater(engine, engine.Logger);
-
-                case PolicyType.Flood:
-                    return new FloodPolicyRater(engine, engine.Logger);
-
-                default:
-                    //currently this can't be reached
-                    return new UnKnownPolicyRater(engine, engine.Logger);
-
+                return (Rater)Activator.CreateInstance(
+                    Type.GetType($"ArdalisRatingService.{policy.Type}PolicyRater"),
+                    new object[] { engine, engine.Logger });
             }
+            catch
+            {
+                return null;
+            }
+            
+            //switch (policy.Type)
+            //{
+            //    case PolicyType.Auto:
+            //        return new AutoPolicyRater(engine, engine.Logger);
+
+            //    case PolicyType.Land:
+            //        return new LandPolicyRater(engine, engine.Logger);
+
+            //    case PolicyType.Life:
+            //        return new LifePolicyRater(engine, engine.Logger);
+
+            //    case PolicyType.Flood:
+            //        return new FloodPolicyRater(engine, engine.Logger);
+
+            //    default:
+            //        //currently this can't be reached
+            //        return new UnKnownPolicyRater(engine, engine.Logger);
+
+            //}
         }
+
+
+
+
+        /*
+         Reflection is introduced so that we dont have to use switch statement inside this class
+         
+         */
     }
 }
